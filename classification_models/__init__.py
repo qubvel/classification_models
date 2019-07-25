0b837be97f43cf5cb93c1e6fc309310cb58d2864
qubvel <qubvel@gmail.com>
2019-07-25 21:36:10 +0300

    Change default _KERAS_* constants

--- classification_models/__init__.py
+++ classification_models/__init__.py
@@ -1,14 +1,10 @@
+import keras_applications as ka
 from .__version__ import __version__
-
-_KERAS_BACKEND = None
-_KERAS_LAYERS = None
-_KERAS_MODELS = None
-_KERAS_UTILS = None
 
 
 def get_submodules_from_kwargs(kwargs):
-    backend = kwargs.get('backend', _KERAS_BACKEND)
-    layers = kwargs.get('layers', _KERAS_LAYERS)
-    models = kwargs.get('models', _KERAS_MODELS)
-    utils = kwargs.get('utils', _KERAS_UTILS)
+    backend = kwargs.get('backend', ka._KERAS_BACKEND)
+    layers = kwargs.get('layers', ka._KERAS_LAYERS)
+    models = kwargs.get('models', ka._KERAS_MODELS)
+    utils = kwargs.get('utils', ka._KERAS_UTILS)
     return backend, layers, models, utils
