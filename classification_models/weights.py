e567f738153d3000e226b74ccf57930c109acacb
hoho2b <38067903+chAwater@users.noreply.github.com>
2019-08-10 13:45:03 +0800

    Fix Resnet18 weights file name

--- classification_models/weights.py
+++ classification_models/weights.py
@@ -56,7 +56,7 @@ WEIGHTS_COLLECTION = [
         'classes': 1000,
         'include_top': False,
         'url': 'https://github.com/qubvel/classification_models/releases/download/0.0.1/resnet18_imagenet_1000_no_top.h5',
-        'name': 'resnet18_imagenet_1000.h5',
+        'name': 'resnet18_imagenet_1000_no_top.h5',
         'md5': '318e3ac0cd98d51e917526c9f62f0b50',
     },
 
